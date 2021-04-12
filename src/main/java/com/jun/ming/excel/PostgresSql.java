bd958736fe9cafbc2f250fefaa3f0f28a1cd1f4c
ZJunMing <591438606@qq.com>
2021-04-12 11:34:00 +0800

    添加忽略文件

--- src/main/java/com/jun/ming/excel/PostgresSql.java
+++ src/main/java/com/jun/ming/excel/PostgresSql.java
@@ -1,5 +1,7 @@
 package com.jun.ming.excel;
 
+import org.apache.commons.collections4.CollectionUtils;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -9,23 +11,20 @@ public class PostgresSql implements SqlInterface {
 	@Override
 	public String createBatchInsertAndUpdateSql(String tableName, String unique, List<String> insertFieldNameList,
 			List<String> updateFieldNameList) {
-		String sql = "INSERT INTO $table_name ($param1) VALUES($param2) ON CONFLICT ( $unique ) DO UPDATE SET $param3";
-		sql = sql.replace("$table_name", tableName);
-		sql = sql.replace("$unique", unique);
-		String params1 = String.join(",", insertFieldNameList);
-		sql = sql.replace("$param1", params1);
-
-		List<String> unnestList = Collections.nCopies(insertFieldNameList.size(), "unnest(?)");
-		String params2 = String.join(",", unnestList);
-		sql = sql.replace("$param2", params2);
-		List<String> updateParam = new ArrayList<>(updateFieldNameList.size());
-		for (String fieldName : updateFieldNameList) {
-			String s = fieldName + "=excluded." + fieldName;
-			updateParam.add(s);
+		String insertSql = createBatchInsertSql(tableName,insertFieldNameList);
+		if(CollectionUtils.isNotEmpty(updateFieldNameList)) {
+			String conflictSql = " ON CONFLICT ( $unique ) DO UPDATE SET $param3";
+			conflictSql = conflictSql.replace("$unique",unique);
+			List<String> updateParam = new ArrayList<>(updateFieldNameList.size());
+			for (String fieldName : updateFieldNameList) {
+				String s = fieldName + "=excluded." + fieldName;
+				updateParam.add(s);
+			}
+			String params3 = String.join(",", updateParam);
+			conflictSql = conflictSql.replace("$param3", params3);
+			insertSql = insertSql+conflictSql;
 		}
-		String params3 = String.join(",", updateParam);
-		sql = sql.replace("$param3", params3);
-		return sql;
+		return insertSql;
 	}
 
 	@Override
